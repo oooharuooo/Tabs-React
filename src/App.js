cdcb0e03e8d5036d5d3a2c077240a57c1cbba92d
Dat <haruthong@gmail.com>
2020-11-29 22:11:47 -0800

    Project completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [isLoading,setIsLoading] = useState(true)
   const [people, setPeople] = useState([]);
   const [index, setIndex] = useState(0);
+  const [isActive,setIsActive] = useState(false)
 
   const fetchData = async () => { 
     const { data } = await axios.get(url);
@@ -25,23 +26,40 @@ function App() {
     )
   }
 
-  const { id, order, title, dates, duties,company } = people[0];
+  const { title, dates, duties, company } = people[index];
 
   return (
 		<section className="section">
 			<div className="title">
-        <h2>Experience</h2>
-        <div className="underline"></div>
+				<h2>Experience</h2>
+				<div className="underline"></div>
 
-        <div className="jobs-center">
+				<div className="jobs-center">
           <div className="btn-container">
-            {people.map(({ id, company }) => <button key={id} className="job-btn">{ company}</button>)}
+						{people.map((people,i) => (
+              <button key={i} className={`job-btn ${i === index && "active"}`} onClick={() => setIndex(i) }>
+								{people.company}
+							</button>
+						))}
           </div>
-          {/* <article className="job-info"></article>
           
-        </div>
+					<article className="job-info">
+						<h3>{title}</h3>
+						<h4>{company}</h4>
+						<p className="job-date">{dates}</p>
+						{duties.map((duties, i) => (
+              <div className="job-desc" key={i}>
+                <FaAngleDoubleRight />
+								<p>{duties}</p>
+							</div>
+						))}
+          </article>
+          
+				</div>
 
-        <button type="button" className="btn">More Info</button>
+				<button type="button" className="btn">
+					More Info
+				</button>
 			</div>
 		</section>
 	);
